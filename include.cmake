c9dca18c585b26c47cc02049daf641ae24581a04
Daniel Dolejška <xdolej08@stud.fit.vutbr.cz>
2018-09-23 09:58:36 +0200

    Úprava CMakeLists pro Linux

--- include.cmake
+++ include.cmake
@@ -1,2 +1,7 @@
 
 include_directories(${ROOT_DIR}/include)
+
+set(CMAKE_DEBUG_POSTFIX                 "d" CACHE STRING "add a postfix, usually d on windows")
+set(CMAKE_RELEASE_POSTFIX               "" CACHE STRING "add a postfix, usually empty on windows")
+set(CMAKE_RELEASEWITHDEBINFO_POSTFIX    "rd" CACHE STRING "add a postfix, usually empty on windows")
+set(CMAKE_MINSIZEREL_POSTFIX            "s" CACHE STRING "add a postfix, usually empty on windows")
